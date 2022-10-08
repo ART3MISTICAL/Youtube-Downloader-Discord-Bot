8ac3b575fe4092a9bca99fda7885e5d5e03105fb
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-10-08 22:22:56 +0530

    commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dis import dis
 import discord
 from discord.ext import commands
 import os
@@ -9,8 +10,17 @@ TOKEN = os.getenv('TOKEN')
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-@bot.event
-async def on_ready():
-	print('Bot is online')
+class abot(discord.Client):
+	def __init__(self):
+		super().__init__(intents=discord.Intents.default())
+		self.synced = False
+
+	async def on_ready(self):
+		await tree.sync(guild=discord.Object(id=776172679731347538))
+		self.sync = True
+		print('Bot is online')
+
+bot = abot()
+tree = app_commands.CommandTree(bot)
 
 bot.run(TOKEN)
